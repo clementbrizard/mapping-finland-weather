ecdeb9b2cac07bec20776e3b219e7f660a9f8071
NF26 Etudiant <nf26p037@smestaens24ab.sme.utc>
2019-06-21 16:48:16 +0200

    Added data insertion scripts for questions 2 and 3

---
+++ scripts/create_temporal_storage.py
@@ -0,0 +1,23 @@
+#%%
+# Connect to keyspace
+from cassandra.cluster import Cluster
+cluster = Cluster()
+session = cluster.connect('finland_weather_metar')
+print("Connection to keyspace done")
+#%%
+# Create table template
+from create_table_temporal import create_table_temporal
+#create_table_temporal(session)
+print("Table creation done")
+
+#%%
+# Load data
+from load_data import load_data
+data = load_data("asos.txt")
+print("Data loading done")
+
+#%%
+# Insert data
+from write_data import write_cassandra
+write_cassandra(session, data)
+print("Data insertion done")
